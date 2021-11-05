4b95e173268bbe057c0f88a1f92e6696180c14ba
amasiye <amasiye313@gmail.com>
2021-11-05 06:03:08 +0200

    refactor: Implement status checking methods
    - Add isOK() method
    - Add isError() method
    - Add value() getter method

--- app/src/Core/Result.php
+++ app/src/Core/Result.php
@@ -6,12 +6,28 @@ use \ArrayObject;
 
 class Result extends ArrayObject
 {
-    public function __construct(
-        protected array $data = []
-    )
-    {
-        parent::__construct( array: $data );
-    }
+  public function __construct(
+    protected array $data = [],
+    protected bool $isOK = true
+  )
+  {
+    parent::__construct( array: $data );
+  }
+
+  public function isOK(): bool
+  {
+    return $this->isOK === true;
+  }
+
+  public function isError(): bool
+  {
+    return $this->isOK === false;
+  }
+
+  public function value(): array
+  {
+    return $this->data;
+  }
 }
 
 ?>
