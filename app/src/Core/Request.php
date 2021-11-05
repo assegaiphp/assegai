b0f2443f33b51fe295591751e71eac15d35a7150
amasiye <amasiye313@gmail.com>
2021-11-05 06:00:24 +0200

    refactor: Moved RequestMethod class to separate file

--- app/src/Core/Request.php
+++ app/src/Core/Request.php
@@ -3,20 +3,6 @@
 namespace LifeRaft\Core;
 
 /**
- * The **RequestMethod** class enumerates the multiple HTTP request methods.
- */
-class RequestMethod
-{
-  const GET = 'GET';
-  const POST = 'POST';
-  const PUT = 'PUT';
-  const PATCH = 'PATCH';
-  const DELETE = 'DELETE';
-  const OPTIONS = 'OPTIONS';
-  const HEAD = 'HEAD';
-}
-
-/**
  * The **Request** class represents the HTTP request and has properties for
  *  the request query string, parameters, HTTP headers, and body
  */
---
+++ app/src/Core/RequestMethod.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LifeRaft\Core;
+
+/**
+ * The **RequestMethod** class enumerates the multiple HTTP request methods.
+ */
+class RequestMethod
+{
+  const GET = 'GET';
+  const POST = 'POST';
+  const PUT = 'PUT';
+  const PATCH = 'PATCH';
+  const DELETE = 'DELETE';
+  const OPTIONS = 'OPTIONS';
+  const HEAD = 'HEAD';
+}
+
+?>
