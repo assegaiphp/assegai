1328442c7d31d16b18fc15c78b7b2e6b80c4f930
amasiye <amasiye313@gmail.com>
2021-11-07 00:10:37 +0200

    feat(authentication): create auth strategies
    * create LocalStrategy class
    * create JWTStrategy class
    * create QAuthStrategy class

---
+++ app/src/Lib/Authentication/Strategies/JWTStrategy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LifeRaft\Lib\Authentication\Strategies;
+
+use LifeRaft\Lib\Authentication\Interfaces\IAuthStrategy;
+
+final class LocalStrategy implements IAuthStrategy
+{
+  public function authenticate(mixed $data, mixed $params): mixed
+  {
+    
+  }
+}
+
+?>
