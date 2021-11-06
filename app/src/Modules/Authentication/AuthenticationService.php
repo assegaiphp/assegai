31275f2007a1ef9482ec148c2d801093acfca175
amasiye <amasiye313@gmail.com>
2021-11-06 06:19:54 +0200

    refactor(Core): update post request handler
    - update business logic

--- app/src/Modules/Authentication/AuthenticationService.php
+++ app/src/Modules/Authentication/AuthenticationService.php
@@ -5,6 +5,10 @@ namespace LifeRaft\Modules\Authentication;
 use LifeRaft\Core\Attributes\Injectable;
 use LifeRaft\Core\Result;
 use LifeRaft\Core\BaseService;
+use LifeRaft\Core\Config;
+use LifeRaft\Lib\Authentication\Authenticator;
+use LifeRaft\Lib\Authentication\Strategies\LocalStrategy;
+use stdClass;
 
 #[Injectable]
 class AuthenticationService extends BaseService
@@ -21,6 +25,26 @@ class AuthenticationService extends BaseService
 
   public function create(mixed $entity): Result
   {
+    $strategy = Config::get('authentication')['default_strategy'];
+
+    if ($entity instanceof stdClass && isset($entity->strategy))
+    {
+      $strategy = $entity->strategy;
+    }
+    else if (is_array(value: $entity) && isset($entity['strategy']))
+    {
+      $strategy = $entity['strategy'];
+    }
+
+    $strategy = match($strategy) {
+      'local' => new LocalStrategy(),
+      'jwt'   => new LocalStrategy(),
+      'oauth' => new LocalStrategy(),
+      default => new LocalStrategy()
+    };
+
+    $authenticator = new Authenticator(strategy: $strategy);
+
     return new Result(data: [$entity]);
   }
 
