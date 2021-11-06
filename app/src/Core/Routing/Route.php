a87556cd98b3c30b284adeb3eb1aba3247bde1c8
amasiye <amasiye313@gmail.com>
2021-11-06 06:38:16 +0200

    feat(routing): create pathmatchingstrategy class
    - update route class

--- app/src/Core/Routing/Route.php
+++ app/src/Core/Routing/Route.php
@@ -1,14 +1,64 @@
 <?php
 
-namespace LifeRaft\Routing;
+namespace LifeRaft\Core\Routing;
+
+use LifeRaft\Core\Interfaces\IModule;
+use LifeRaft\Core\Routing\Types\PathMatchingStrategy;
 
 class Route
 {
+  /**
+   * @param string $path 
+   * @param null|PathMatchingStrategy $path The path-matching strategy, one of 'prefix' or 'full'.
+   * Default is 'prefix'.
+   * @param null|IModule $module The module to load when the path matches
+   */
   public function __construct(
-    protected string $path = '/',
-    protected mixed $controller = null 
+    protected ?string $path = null,
+    protected ?PathMatchingStrategy $pathMatchingStrategy = null,
+    protected ?IModule $module = null,
+    protected ?string $redirectTo = null,
+    protected array $canActivate = [],
   )
   {
+    if (is_null($this->pathMatchingStrategy))
+    {
+      $this->pathMatchingStrategy = PathMatchingStrategy::PREFIX();
+    }
+  }
+
+  protected function canMatch(): bool
+  {
+    $canMatch = false;
+
+    // TODO: Implement canMatch logic
+
+    return $canMatch;
+  }
+
+  public function canRedirect(): bool
+  {
+    return !is_null($this->redirectTo);
+  }
+
+  protected function canActivate(): bool
+  {
+    $canActivate = true;
+
+    foreach ($this->canActivate as $guard)
+    {
+      if (!$guard->canActivate())
+      {
+        $canActivate = false;
+      }
+    }
+
+    return $canActivate;
+  } 
+
+  public function isActive(): bool
+  {
+    return $this->canMatch() && $this->canActivate();
   }
 }
 
---
+++ app/src/Core/Routing/Types/PathMatchingStrategy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LifeRaft\Core\Routing\Types;
+
+final class PathMatchingStrategy
+{
+  public function __construct(
+    private string $value
+  ) { }
+
+  public function __toString(): string
+  {
+    return $this->value;
+  }
+
+  public static function PREFIX(): PathMatchingStrategy
+  {
+    return new PathMatchingStrategy(value: 'prefix');
+  }
+
+  public static function FULL(): PathMatchingStrategy
+  {
+    return new PathMatchingStrategy(value: 'full');
+  }
+}
+
+?>
