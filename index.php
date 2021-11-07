175cd97a0e878a5ee562d8f8eccd760ba37b43be
amasiye <amasiye313@gmail.com>
2021-11-07 16:15:51 +0200

    refactor(routing): update index.php
    * create $router instance and inject into app

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 use LifeRaft\Core\App;
 use LifeRaft\Core\Request;
+use LifeRaft\Core\Routing\Router;
 
 require_once 'app/bootstrap.php';
 
 $request = new Request();
-$app = new App( request: $request, config: $config );
+$router = new Router();
+$app = new App( request: $request, router: $router, config: $config );
 $app->run();
