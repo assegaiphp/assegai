ee1832093df7b281a526b031e08a3b563bce987f
amasiye <amasiye313@gmail.com>
2021-11-11 14:17:24 +0200

    feature(core): add interfaces
    * add IArraySerializable interface
    * add IJSONSerializable interface

---
+++ app/src/Core/Interfaces/IJSONSerializable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Assegai\Core\Interfaces;
+
+interface IJSONSerializable
+{
+  public function toJSON(): string;
+
+  public function fromJSON(string $jsonString): mixed;
+}
+
+?>
