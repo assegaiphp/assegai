06bcd74694126eef3559fb7b6a6f97a8c26386b9
amasiye <amasiye313@gmail.com>
2021-12-18 23:01:55 +0200

    feat(tests): add HttpService test
    * add post request test

--- tests/HttpServiceTest.php
+++ tests/HttpServiceTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Assegai\Lib\Network\HttpRequestOptions;
 use Assegai\Lib\Network\HttpService;
 use PHPUnit\Framework\TestCase;
 
@@ -12,7 +13,19 @@ final class HttpServiceTest extends TestCase
     $this->assertTrue(condition: $response->isOK());
   }
 
-  public function testPostRequest(): void {}
+  public function testPostRequest(): void {
+    $http = new HttpService();
+    $options = new HttpRequestOptions(
+      body: [
+        "name" => "Assegai",
+        "version" => 1,
+        "saySomething" => "I'm giving up on you"
+      ]
+    );
+    $response = $http->post(url: 'https://jsonplaceholder.typicode.com/posts', options: $options);
+
+    $this->assertTrue(condition: $response->isOK());
+  }
 
   public function testPutRequest(): void {}
 
@@ -21,4 +34,3 @@ final class HttpServiceTest extends TestCase
   public function testDeleteRequest(): void {}
 }
 
-?>
