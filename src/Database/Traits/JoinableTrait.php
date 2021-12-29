f63f90c3c00bd15be03d5bf66088288bad4909ac
Andrew Masiye <andrew.masiye@liferaftinc.com>
2021-12-29 20:41:41 +0200

    Change to JoinType class to enum with PHP 8.1 @amasiye Fixes #17

--- src/Database/Traits/JoinableTrait.php
+++ src/Database/Traits/JoinableTrait.php
@@ -10,29 +10,28 @@ trait JoinableTrait
 
   public function join(array|string $tableReferences): SQLJoinExpression
   {
-    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::JOIN());
+    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::JOIN);
   }
 
   public function leftJoin(array|string $tableReferences): SQLJoinExpression
   {
-    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::LEFT_JOIN());
+    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::LEFT_JOIN);
   }
 
   public function rightJoin(array|string $tableReferences): SQLJoinExpression
   {
-    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::RIGHT_JOIN());
+    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::RIGHT_JOIN);
   }
 
   public function innerJoin(array|string $tableReferences): SQLJoinExpression
   {
-    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::INNER_JOIN());
+    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::INNER_JOIN);
   }
 
   public function outerJoin(array|string $tableReferences): SQLJoinExpression
   {
-    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::OUTER_JOIN());
+    return new SQLJoinExpression(query: $this->query, joinTableReferences: $tableReferences, joinType: JoinType::OUTER_JOIN);
   }
-
 }
 
 ?>
