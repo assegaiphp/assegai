a2aa8cb35aef8637e861e5a1eb5fb5b94c502e2f
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-01-02 15:41:42 +0200

    Add authentication strategy validation to Authenticator Fixes #56

--- src/Lib/Authentication/Authenticator.php
+++ src/Lib/Authentication/Authenticator.php
@@ -2,19 +2,35 @@
 
 namespace Assegai\Lib\Authentication;
 
+use Assegai\Core\Config;
 use Assegai\Lib\Authentication\Interfaces\IAuthStrategy;
+use Assegai\Lib\Authentication\Strategies\JWTStrategy;
+use Assegai\Lib\Authentication\Strategies\LocalStrategy;
+use Assegai\Lib\Authentication\Strategies\OAuthStrategy;
 
 /**
- * The `Authenticator` class houses methods for issuing and managing user 
+ * The `Authenticator` class houses methods for granging and managing user 
  * authentication.
  */
 final class Authenticator
 {
+  private array $strategies = [
+    'local' => LocalStrategy::class,
+    'jwt' => JWTStrategy::class,
+    'oauth' => OAuthStrategy::class
+  ];
+
   public function __construct(
     private ?IAuthStrategy $strategy = null
   )
   {
-    
+    $strategies = Config::get('authentication')['strategies'] ?? [];
+    $this->strategies = array_merge($this->strategies, $strategies);
+  }
+
+  public function getStrategy(string $name): IAuthStrategy|false
+  {
+    return $this->strategies[$name] ?? false;
   }
 }
 
