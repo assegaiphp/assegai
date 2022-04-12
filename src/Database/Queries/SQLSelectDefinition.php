c6911a764f1cf41c5281e42aa2ace340a7c28dc3
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-04-12 16:53:04 +0200

    refactor(database): add sql aggregate function support
    - add count method
    - add avg method
    - add sum method

--- src/Database/Queries/SQLSelectDefinition.php
+++ src/Database/Queries/SQLSelectDefinition.php
@@ -13,25 +13,68 @@ final class SQLSelectDefinition
 
   public function all(array $columns = []): SQLSelectExpression
   {
-    $queryString = "";
+    $queryString = $this->getColumnListString(columns: $columns);
+
+    $this->query->appendQueryString($queryString);
+
+    return new SQLSelectExpression( query: $this->query );
+  }
+
+  public function count(array $columns = []): SQLSelectExpression
+  {
+    $queryString = "COUNT(" . $this->getColumnListString(columns: $columns) . ')';
+
+    $this->query->appendQueryString($queryString);
+
+    return new SQLSelectExpression( query: $this->query );
+  }
+
+  public function avg(array $columns = []): SQLSelectExpression
+  {
+    $queryString = "AVG(" . $this->getColumnListString(columns: $columns) . ')';
+
+    $this->query->appendQueryString($queryString);
+
+    return new SQLSelectExpression( query: $this->query );
+  }
+
+  public function sum(array $columns = []): SQLSelectExpression
+  {
+    $queryString = "SUM(" . $this->getColumnListString(columns: $columns) . ')';
+
+    $this->query->appendQueryString($queryString);
+
+    return new SQLSelectExpression( query: $this->query );
+  }
+
+  /**
+   * Creates and returns a list of comma-separatred column names from a given 
+   * array of strings.
+   * 
+   * @param array<string> $columns The list of column names.
+   * 
+   * @return string Returns a list of comma-separated column names if the 
+   * given array is not empty, otherwise returns `*`.
+   */
+  private function getColumnListString(array $columns): string
+  {
+    $columnListString = '';
     $separator = ', ';
 
     if (empty($columns))
     {
-      $queryString .= "*";
+      $columnListString .= "*";
     }
     else
     {
       foreach ($columns as $key => $value)
       {
-        $queryString .= is_numeric($key) ? "${value}${separator}" : "$value as ${key}${separator}";
+        $columnListString .= is_numeric($key) ? "${value}${separator}" : "$value as ${key}${separator}";
       }
     }
-    $queryString = trim($queryString, $separator);
+    $columnListString = trim($columnListString, $separator);
 
-    $this->query->appendQueryString($queryString);
-
-    return new SQLSelectExpression( query: $this->query );
+    return $columnListString;
   }
 }
 
